65e94598313755ef0a3b36948e995d02af160cf0
a.evtuh <alexander.evtukh@orangesoft.co>
2024-11-01 14:32:27 +0300

    clear code

--- navikit-demo/domain/src/main/java/com/yandex/navikitdemo/data/PlayerManagerImpl.kt
+++ navikit-demo/domain/src/main/java/com/yandex/navikitdemo/data/PlayerManagerImpl.kt
@@ -1,22 +1,17 @@
 package com.yandex.navikitdemo.data
 
 import android.content.Context
-import android.media.MediaPlayer
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import com.yandex.navikitdemo.domain.PlayerManager
 import com.yandex.navikitdemo.domain.models.LocalToken
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.Job
 import javax.inject.Inject
 
 class PlayerManagerImpl @Inject constructor(
     @ApplicationContext context: Context,
 ) : PlayerManager {
     private val player: ExoPlayer by lazy { ExoPlayer.Builder(context).build() }
-    private val mediaPlayer = MediaPlayer()
-
-    private var playListJob: Job? = null
 
     override fun play(queue: List<LocalToken>) {
         player.stop()
@@ -27,7 +22,7 @@ class PlayerManagerImpl @Inject constructor(
     }
 
     override fun reset() {
-        playListJob?.cancel()
-        mediaPlayer.reset()
+        player.stop()
+        player.clearMediaItems()
     }
 }
